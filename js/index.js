004a3a9600281530e144240e6587a4586689303f
James Hughes <heezmagnif@gmail.com>
2016-11-21 12:32:40 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,80 @@
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+  $ele.fadeIn(1000).delay(6000).fadeOut(1000, function() {
+    var $next = $(this).next('.quote');
+    fade($next.length > 0 ? $next : $(this).parent().children().first());
+  });
+}
+fade($('.quoteLoop > .quote').first());
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function() {
+
+  if ($(window).scrollTop() > 300) {
+    $('.main_nav').addClass('sticky');
+  } else {
+    $('.main_nav').removeClass('sticky');
+  }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+  if ($('.main_nav').hasClass('open-nav')) {
+    $('.main_nav').removeClass('open-nav');
+  } else {
+    $('.main_nav').addClass('open-nav');
+  }
+});
+
+$('.main_nav li a').click(function() {
+  if ($('.main_nav').hasClass('open-nav')) {
+    $('.navigation').removeClass('open-nav');
+    $('.main_nav').removeClass('open-nav');
+  }
+});
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+jQuery(document).ready(function($) {
+
+  $('.smoothscroll').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash,
+      $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+
+});
+
+var modal = document.getElementById('myModal');
+var img = document.getElementById('myImg');
+var captionText = document.getElementsByClassName('caption')[0];
+img.onclick = function() {
+  modal.style.display = 'block';
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName('close')[0];
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+var tween = TweenMax.staggerFrom(".heading", 0.8, {
+  opacity: 0,
+  y: 20,
+  delay: 0.2
+}, 0.4);
